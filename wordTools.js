7d185a9762620161befab426c944a0a003715f90
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-27 22:36:14 +0000

    Update wordTools.js

--- wordTools.js
+++ wordTools.js
@@ -1,7 +1,42 @@
-const wordlist = require('wordlist-english')['english'];
-const anagrams = require('anagrams');
+const readline = require('readline');
 
-function findAnagrams(letters){
-    return wordlist.filter(word => anagrams(letters, word));
+function askForWord() {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    rl.question('Enter letters: ', (letters) => {
+      rl.close();
+      resolve(letters);
+    });
+  });
 }
-module.exports = { findAnagrams };
+
+function chooseOne(options) {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    console.log('Choose one of the following options:');
+    options.forEach((option, index) => {
+      console.log(`${index + 1}: ${option}`);
+    });
+
+    rl.question('Enter the number of your choice: ', (choice) => {
+      rl.close();
+      const index = parseInt(choice, 10) - 1;
+      if (index >= 0 && index < options.length) {
+        resolve(options[index]);
+      } else {
+        console.log('Invalid choice, please try again.');
+        resolve(chooseOne(options)); // Recursively call if invalid choice
+      }
+    });
+  });
+}
+
+module.exports = { askForWord, chooseOne };
