0b71d499b9c7f81a63546cad1553c24d7f74d37c
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-27 21:26:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('test');
+const userInput = require('./userInput');
+const gridOperations = require('./gridOperations');
+//const wordlist = require('./wordlist');
+
+async function main(){
+  console.log('Welcome to the word game!')
+    const word = await userInput.askForWord();
+
+  
+  console.log(`Word: ${word}`);
+}
+main().then(()=>(console.log('End of the game')));
