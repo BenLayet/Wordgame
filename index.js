63e8372fab5ed94f3c91792c432a6d06084180bc
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-27 22:19:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const gridOperations = require('./gridOperations');
 const wordTools = require('./wordTools');
 
 async function main(){
-  console.log('Welcome to the word game!')
-    const word = await  userInput.askForWord();
+  console.log('Welcome to the word game!');
+  const word = await userInput.askForWord();
 
   const anagrams = wordTools.findAnagrams(word);
   
   console.log(`Word: ${word}`);
+  console.log(`Anagrams: ${anagrams.join(', ')}`);
+
+console.log('End of the game')
 }
-main().then(()=>(console.log('End of the game')));
+
+main();
