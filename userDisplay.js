200ba3988ffaa4c7e88e6f37b57a574dde4e234e
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 17:31:33 +0000

    Update userDisplay.js

--- userDisplay.js
+++ userDisplay.js
@@ -4,15 +4,29 @@ function showMessage(message) {
   console.log(message);
 }
 
+function showGrid(grid){
+    showMessage(grid.map(row => row.join(' ')).join('\n'));
+    showMessage('---------------')
+}
+
+function showChallenge(challenge) {
+  showMessage(`Challenge:`);
+  showMessage ('Initial Grid:');
+  showGrid(challenge.initialGrid);
+  showMessage(`target: ${challenge. target}`);
+}
+
+function showSolution(solution) {
+  showMessage(`Solution: ${solution}`);
+}
 
 function showAnalysis(analysis) {
   showChallenge(analysis.challenge);
-  if(analysis.hasSolution){
+  if (analysis.hasSolution) {
     showSolution(analysis.solution);
-  }else{
+  } else {
     showMessage('No solution found');
   }
- 
 }
 
 module.exports = { showMessage, showAnalysis };
