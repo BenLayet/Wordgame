560dc4080dad1f4f2191d3e5b874ff63123ee9a1
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 11:08:58 +0000

    Update wordTools.js

--- wordTools.js
+++ wordTools.js
@@ -1,7 +1,7 @@
 const wordlist = require('wordlist-english')['english'];
-const Trie = require('trie-prefix-tree2');
+const trie = require('trie-prefix-tree2').trie;
 
-const allWordsTrie = Trie(wordlist);
+const allWordsTrie = trie(wordlist);
 
 function findAnagrams(letters) {
     return allWordsTrie.getAnagrams(letters);
