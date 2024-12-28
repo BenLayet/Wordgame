349ae04388d076ba74725f6f3cb1eede1a545aca
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 11:34:20 +0000

    Update cheatTed.js

--- cheatTed.js
+++ cheatTed.js
@@ -4,11 +4,14 @@ async function cheatTed(userInput, wordTools) {
 
   const anagrams = wordTools.findAnagrams(letters);
 
+assert(anagrams.length>0,'No anagram found');
   const word = await userInput.chooseOne(anagrams);
  
   console.log(`Anagram: ${word}`);
 
   console.log('End of the game');
 }
+
+
 
 module.exports = { cheatTed };
