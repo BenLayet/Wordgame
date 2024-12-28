2363676489fee0e73ad3a3aac773ed2ceef6f7a3
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 01:06:01 +0000

    Update wordTools.js

--- wordTools.js
+++ wordTools.js
@@ -1,21 +1,7 @@
-const readline = require('readline');
+const wordlist = require('wordlist-english')['english'];
+const anagrams = require('anagrams');
 
-function askForWord() {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    rl.question('Enter letters: ', (letters) => {
-      rl.close();
-      resolve(letters);
-    });
-  });
-}
 function findAnagrams(letters){
     return wordlist.filter(word => anagrams(letters, word));
 }
-
-
-module.exports = { askForWord, findAnagrams};
+module.exports = { findAnagrams };
