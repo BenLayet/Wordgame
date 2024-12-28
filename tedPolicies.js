814aa03cc00aef59bdf9448de72836c267ae87bf
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 11:54:47 +0000

    Update tedPolicies.js

--- tedPolicies.js
+++ tedPolicies.js
@@ -1,3 +1,9 @@
-function calculatePoints(word){
-    return word.length * word.length;
+function calculatePoints(word) {
+  return word.length * word.length;
 }
+
+function sortByPointsAscending(words) {
+  return words.sort((a, b) => calculatePoints(a) - calculatePoints(b));
+}
+
+module.exports = { calculatePoints, sortByPointsAscending };
