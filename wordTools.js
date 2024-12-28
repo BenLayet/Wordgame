cd685b57137fc6790423a60361a6708a1cbf29bb
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 01:00:48 +0000

    Update wordTools.js

--- wordTools.js
+++ wordTools.js
@@ -13,30 +13,9 @@ function askForWord() {
     });
   });
 }
-
-function chooseOne(options) {
-  return new Promise((resolve) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    console.log('Choose one of the following options:');
-    options.forEach((option, index) => {
-      console.log(`${index + 1}: ${option}`);
-    });
-
-    rl.question('Enter the number of your choice: ', (choice) => {
-      rl.close();
-      const index = parseInt(choice, 10) - 1;
-      if (index >= 0 && index < options.length) {
-        resolve(options[index]);
-      } else {
-        console.log('Invalid choice, please try again.');
-        resolve(chooseOne(options)); // Recursively call if invalid choice
-      }
-    });
-  });
+function findAnagrams(letters){
+    return wordlist.filter(word => anagrams(letters, word));
 }
 
-module.exports = { askForWord, chooseOne };
+
+module.exports = { askForWord, findAnagrams};
