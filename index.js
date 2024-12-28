3ca6aa94d6661fe4274098628d509bbf8495502e
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 11:40:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const gridOperations = require('./gridOperations');
 const wordTools = require('./wordTools');
 const cheatTed = require('./cheatTed').cheatTed;
 
-
-cheatTed(userInput, wordTools, gridOperations);
+cheatTed(userInput, wordTools, gridOperations).catch(error => {
+  console.error('An error occurred:', error.message);
+});
