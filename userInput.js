f65269dd7497865a41f850654f3b06248cdd440c
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 01:01:12 +0000

    Update userInput.js

--- userInput.js
+++ userInput.js
@@ -13,5 +13,28 @@ function askForWord() {
     });
   });
 }
+function chooseOne(options) {
+  return new Promise((resolve) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-module.exports = { askForWord };
+    console.log('Choose one of the following options:');
+    options.forEach((option, index) => {
+      console.log(`${index + 1}: ${option}`);
+    });
+
+    rl.question('Enter the number of your choice: ', (choice) => {
+      rl.close();
+      const index = parseInt(choice, 10) - 1;
+      if (index >= 0 && index < options.length) {
+        resolve(options[index]);
+      } else {
+        console.log('Invalid choice, please try again.');
+        resolve(chooseOne(options)); // Recursively call if invalid choice
+      }
+    });
+  });
+}
+module.exports = { askForWord, chooseOne };
