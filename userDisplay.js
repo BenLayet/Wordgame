c24c98ab0fe086937f2e362f51d2f42275c508dc
BenLayet <7203461+BenLayet@users.noreply.github.com>
2024-12-28 21:12:18 +0000

    Update userDisplay.js

--- userDisplay.js
+++ userDisplay.js
@@ -36,8 +36,6 @@ function showSolution(solution) {
 
 
 function showAnalysis(analysis) {
-  showMessage(JSON.stringify(analysis,'  '));
- 
   showChallenge(analysis.challenge);
   if (analysis.solution.found) {
     showSolution(analysis.solution);
