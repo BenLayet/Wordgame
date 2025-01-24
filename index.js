82591b9fb2de9ad1cac67740630e14ec6e0e7321
BenLayet <layetbenjamin@gmail.com>
2025-01-24 19:46:57 +0100

    Updates

--- index.js
+++
@@ -1,9 +0,0 @@
-const userInput = require('./userInput');
-const gridOperations = require('./gridOperations');
-const wordTools = require('./wordTools');
-const userDisplay = require('./userDisplay');
-const cheatTed = require('./cheatTed').cheatTed;
-
-cheatTed(userInput, wordTools, gridOperations, userDisplay).catch(error => {
-  console.error('An error occurred:', error);
-});
--- tedPolicies.js
+++
@@ -1,9 +0,0 @@
-function calculatePoints(word) {
-  return word.length * word.length;
-}
-
-function sortByPointsAscending(words) {
-  return words.sort((a, b) => calculatePoints(a) - calculatePoints(b));
-}
-
-module.exports = { calculatePoints, sortByPointsAscending };
